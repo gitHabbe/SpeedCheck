689ae09a825acd62214f3bb1efb7385c7bf56ea5
gitHabbe <91habbe@gmail.com>
2019-07-29 17:57:13 +0200

    more testing

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import style from '../style/style.css'
+import style from "../style/style.css";
 
 const Home = () => {
-    return (
-        <div className={style.home}>
-            This is the homepage
-        </div>
-    );
+    return <div className={style.home}>This is the homepage</div>;
 };
 
-export default Home
+export default Home;
